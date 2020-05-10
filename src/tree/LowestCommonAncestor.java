1e51f5f4ddd666402824daa9d1867f4ccb741d42
六幻 <longfei.wlf@alibaba-inc.com>
2020-05-10 19:30:20 +0800

    公共祖先

---
+++ src/tree/LowestCommonAncestor.java
@@ -0,0 +1,42 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package tree;
+
+/**
+ *
+ * 最近公共祖先
+ * 
+ * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/236-er-cha-shu-de-zui-jin-gong-gong-zu-xian-hou-xu/
+ *
+ * @author longfei.wlf
+ * @version $Id: LowestCommonAncestor.java, v 0.1 2020-05-10 12:34 PM longfei.wlf Exp $$
+ */
+public class LowestCommonAncestor {
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int      val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+}
